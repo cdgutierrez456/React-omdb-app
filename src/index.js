e29b4ea093392b44a6723578f465b9d719c2fd57
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-06-26 16:54:53 -0500

    Haciendo uso de eslint para analisar detalles de codigo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
 
 
@@ -8,15 +8,22 @@ import 'bootswatch/dist/cyborg/bootstrap.min.css'
 
 const App = () => {
     return (
-        <main>
-            <div className='container'>
-                <List/>
-            </div>
-        </main>
+        <Fragment>
+            <nav className="navbar navbar-dark bg-dark border-bottom border-white">
+                <a href="/" className='navbar-brand'>
+                    MovieApp
+                </a>
+            </nav>
+            <main>
+                <div className='container'>
+                    <List />
+                </div>
+            </main>
+        </Fragment>
     )
 }
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 
 
